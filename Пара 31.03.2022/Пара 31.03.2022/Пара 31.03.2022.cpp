72606f8f412c6d7057e3d75f14d769c0ee4d8aee
maksi <maksi@DESKTOP-OD89I65>
2022-04-07 00:19:58 +0300

    Добавьте файлы проекта.

---
+++ Пара 31.03.2022/Пара 31.03.2022/Пара 31.03.2022.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+pair <int, int> sw (int a, int b) 
+{
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	
+	return pair <int, int> (a, b);
+}
+
+
+int main() {
+	int a = 0, b = 0;
+	setlocale(0, "");
+
+	cout << "Введите 2 числовых значения\n";
+
+	cin >> a >> b;
+	//  Смена значений по переменным
+	/*a = a + b;
+	b = a - b;
+	a = a - b;   */          
+
+	auto p = sw(a, b);
+	cout << p.first << " " << p.second;
+	cout << endl;
+	system("pause");
+}
