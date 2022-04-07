53e2672c35aacdb70797b0a6c3df0892516653e6
Olfad0 <57636793+Olfad0@users.noreply.github.com>
2022-04-07 23:22:20 +0300

    работа на парах 07.04.2022

---
+++ Пара 07.04.2022 Задание 3/Пара 07.04.2022 Задание 3/Пара 07.04.2022 Задание 3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    float x = 0, y = 0, z = 0, a = 0, k = 0, b = 0;
+    cout << "Введите значения x, a, k, b\n";
+    cin >> x >> a >> k >> b;
+
+    y = sqrt(1 + pow(x, 2));
+    z = a + sin(k * x + b);
+    
+    if (y > z) 
+        cout << "Число y больше числа z и оно равно: " << y << endl;
+    else
+        cout << "Число z больше числа y и оно равно: " << z << endl;
+
+    system("pause");
+    return(0);
+}
+
---
+++ Пара 07.04.2022 Задание 4/Пара 07.04.2022 Задание 4/Пара 07.04.2022 Задание 4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int year = 0, days = 0 ;
+
+	cout << "Введите год\n";
+	cin >> year;
+
+	if (year % 4 == 0 or (year % 100 == 0 && year % 400 == 0))
+	{
+		days = 366;
+		cout << "Год " << year << " является весокосным и количество дней равно: " << days << endl;
+	}
+	else
+	{
+		days = 365;
+		cout << "Год " << year << " не является весокосным и количество дней равно: " << days << endl;
+	}
+	system("pause");
+	return(0);
+}
+
